c5b707bfbcb2016d4e24f6b49646eeb5a5aa2e28
Lars Zwaan <lars1095@hotmail.com>
2022-01-12 14:20:55 +0100

    added class functionality & process simplification

--- setup_test.py
+++ setup_test.py
@@ -7,36 +7,38 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
+# GENERAL CLASS COMMENT:
+# registry part is necessary to easily loop over all instances of the object later
+
 # a class that represents a House object
-# i now load in the locations of each house; i should make objects for each of m still
-class House:
+class House(object):
+    _registry = []
+
     def __init__(self, x, y, maxoutput):
+        self._registry.append(self)
         self.x = x
         self.y = y
         self.maxoutput = maxoutput
 
 # a class that represents a Battery object
-# i now load in the locations of each battery; i should make objects for each of m still
 class Battery:
+    _registry = []
+
     def __init__(self, x, y, capacity):
+        self._registry.append(self)
         self.x = x
         self.y = y
         self.capacity = capacity
 
-# a class that represents a Cable_segment object
+# a class that represents a Cable object
 # havent done anything with this yet
-class Cable_segment:
+class Cable:
+    _registry = []
+
     def __init__(self, x, y):
+        self._registry.append(self)
         self.x = x
         self.y = y
-
-    # this is just a test for me (LZ) to get familiar with classes again
-    def talk(self):
-        print("There is a cable segment on (x, y) = ({}, {})".format(self.x, self.y))
-
-# this is just a test for me (LZ) to get familiar with classes again
-# small_cable = Cable_segment(69, 69)
-# small_cable.talk()
 
 # load in the locations of the houses and batteries as provided
 def load_district(dis_id):
@@ -44,35 +46,39 @@ def load_district(dis_id):
     df_bat = pd.read_csv("data/district_{0}/district-{0}_batteries.csv".format(dis_id))
     df_hou = pd.read_csv("data/district_{0}/district-{0}_houses.csv".format(dis_id))
 
-    bat_coords = []
-    hou_coords = []
-
-    # make lists of coords (comma separated string -> list of [x, y] ints)
-    for bat in df_bat['positie']:
-        bat_coords.append(list(map(int, bat.split(','))))
+    # make objects of each instance in the dataframe
+    for idx, bat in df_bat.iterrows():
+        # comma separated string -> list of [x, y] ints
+        coord_pair = list(map(int, bat['positie'].split(',')))
+        battery = Battery(coord_pair[0], coord_pair[1], bat['capaciteit'])
     
     for idx, hou in df_hou.iterrows():
-        hou_coords.append([int(hou['x']), int(hou['y'])])
+        # comma separated string -> list of [x, y] ints
+        coord_pair = [int(hou['x']), int(hou['y'])]
+        house = House(coord_pair[0], coord_pair[1], hou['maxoutput'])
 
-    # make lists into arrays to be able to Transpose later
-    # could be more efficient but ok
-    bat_coords = np.array(bat_coords)
-    hou_coords = np.array(hou_coords)
-
-    return bat_coords, hou_coords
+    return
 
 # draw a basic visualisation of the provided elements
-def draw_grid(bat_coords, hou_coords):
+def draw_grid():
 
-    # transpose arrays to make m in right order to scatter
-    bat_coords = bat_coords.T
-    hou_coords = hou_coords.T
+    bat_coords = [[], []]
+    hou_coords = [[], []]
+
+    # gather coords of all object instances and format in scatterable way
+    for hou in House._registry:
+        hou_coords[0].append(hou.x)
+        hou_coords[1].append(hou.y)
+
+    for bat in Battery._registry:
+        bat_coords[0].append(bat.x)
+        bat_coords[1].append(bat.y)
 
     fig, ax = plt.subplots()
-
     ax.scatter(bat_coords[0], bat_coords[1], c='red', label="batteries")
     ax.scatter(hou_coords[0], hou_coords[1], c='blue', label="houses")
 
+    # just draw a random line to see how it looks
     x = [0, 1, 2, 3, 4, 5]
     y = [0, 0, 0, 0, 0, 0]
     ax.plot(x, y, c='green')
@@ -88,5 +94,5 @@ def draw_grid(bat_coords, hou_coords):
 
     return
 
-# do the process for district (1) / (2) / (3)
-draw_grid(load_district(2)[0], load_district(2)[1])
+load_district(2)
+draw_grid()
